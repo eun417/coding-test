d695981681b380b15c27f1d09a136468fdf94c73
eun417 <126125547+eun417@users.noreply.github.com>
2023-12-11 14:29:24 +0000

    [백준] DFS/BFS(단지 번호 붙이기)🅾
    실버1, 2667번

--- algorithm/DFS_BFS/BJ/bj_18.py
+++ algorithm/DFS_BFS/BJ/bj_18.py
@@ -1,57 +1,71 @@
-#[백준] 단지 번호 붙이기 (실버1) 231208 ❎
-#안 풀린다 ... 나중에 다시 풀기
+#[백준] 단지 번호 붙이기 (실버1) 231208 ❎ 231211 🅾
+#231208/ 안 풀린다 ... 나중에 다시 풀기
 #총 단지 수는 구했는데, 각 단지내 집의수를 못 구함
+#231211/ 풀었다... GPT의 도움을 받아서 제출은 하지 않음 !!
 def solution():
+  import sys
+  sys.setrecursionlimit(10**5)  #재귀 깊이 제한 변경
+
   n = int(input())  #지도의 크기
 
-  graph = []
+  graph = []  #아파트 단지
   for _ in range(n):
     graph.append(list(map(int, input())))
 
-  # print(graph)
+  complex_count = 0  #총 단지 수 (*결과값)
+  house_count = []  #각 단지내 집의 수 (*결과값)
 
-  #총 단지 수 (*결과값)
-  complex = 0
+  #방향 벡터
+  dx = [-1, 0, 1, 0]
+  dy = [0, -1, 0, 1]
 
   #DFS
   def dfs(x, y):
-    global complex
+    global complex_count
+    global house_count
 
     if x <= -1 or x >= n or y <= -1 or y >= n:
       return False
 
     if graph[x][y] == 1:
-      graph[x][y] = 2
-      dfs(x - 1, y)
-      dfs(x, y - 1)
-      dfs(x + 1, y)
-      dfs(x, y + 1)
-      # for i in range(n):
-      #   for j in range(n):
-      #     print(graph[i][j], end=' ')
-      #   print()
-      # print('-----------------')
+      graph[x][y] = 2  # 방문한 집을 2로 표시
+      house_count[complex_count] += 1  #각 단지내 집의 수 카운트
+      for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        dfs(nx, ny)
       return True
 
     return False
 
-  for i in range(n):
-    for j in range(n):
-      if dfs(i, j) == True:
-        complex += 1
+  #house_count 리스트 초기화
+  for _ in range(n):
+    house_count.append(0)
 
   for i in range(n):
     for j in range(n):
-      print(graph[i][j], end=' ')
-    print()
-  print('complex:', complex)
+      if dfs(i, j):
+        complex_count += 1
 
+  print(complex_count)
+
+  for i in range(complex_count):
+    print(house_count[i])
+
+  # for i in range(n):
+  #   for j in range(n):
+  #     print(graph[i][j], end=' ')
+  #   print()
+
+  # #방향 벡터
   # dx = [-1, 0, 1, 0]
   # dy = [0, -1, 0, 1]
 
   # #BFS
   # def bfs(x, y) :
   #   from collections import deque
+
+  #   count = 0
 
   #   queue = deque()
   #   queue.append((x, y))
@@ -69,15 +83,20 @@ def solution():
   #         continue
 
   #       if graph[nx][ny] == 1:
-  #         graph[nx][ny] = graph[x][y]
+  #         graph[nx][ny] = count
   #         queue.append((nx, ny))
-  #         print('queue:', queue)
-  #         for i in range(n) :
-  #           for j in range(n) :
-  #             print(graph[i][j], end=' ')
-  #           print()
-  #       complex
+  #         count += 1
 
-  #   return graph[x][y]
+  #   return count
 
-  # print(bfs(0, 0))
+  # for i in range(n):
+  #   for j in range(n):
+  #     bfs(i, j)
+
+  # for i in range(n):
+  #   for j in range(n):
+  #     print(graph[i][j], end=' ')
+  #   print()
+
+  # print('all_count:', all_count)
+  # print('count:', count)
