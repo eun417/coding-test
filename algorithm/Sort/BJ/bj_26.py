1cfabe0ec493badac7f9a3b1b17aa61179c313fe
eun417 <126125547+eun417@users.noreply.github.com>
2023-12-15 04:59:15 +0000

    [백준] 정렬(숫자 카드2)✳
    다른 사람 코드 참고

--- algorithm/Sort/BJ/bj_26.py
+++ algorithm/Sort/BJ/bj_26.py
@@ -1,18 +1,24 @@
-#숫자 카드 2 (실버4) 231215 ✅ ... 시간 초과
+#숫자 카드 2 (실버4) 231215 ✳
+#다른 사람 코드 참고함 ... 제출 X
 def solution() :
   import sys
   input = sys.stdin.readline
   
   n = int(input())  #상근이가 가지고 있는 숫자 카드 개수
-  cards = list(map(int, input().split()))  #숫자 카드에 적힌 정수
+  cards = list(map(int, input().split()))  
 
   m = int(input())  #상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 수
   target = list(map(int, input().split()))  
 
-  cards_count = [0] * len(target)
+  cards_count = {}  #카드 개수 저장할 딕셔너리
+  for i in target:
+    #target의 각 숫자에 대해 카드 개수를 0으로 초기화
+    cards_count[i] = 0
 
-  for i in range(len(target)) :
-    if target[i] in cards :  #target인 카드가 cards 안에 있다면
-      cards_count[i] = cards.count(target[i])  #그 카드 개수 세기
+  for i in cards:
+    if i in cards_count:  #만약 딕셔너리 안에 겹치는 cards가 있다면
+      cards_count[i] += 1  #카드 개수 +1
 
-  print(*cards_count)
+  #결과 출력
+  for i in cards_count:
+    print(cards_count[i], end=' ')
